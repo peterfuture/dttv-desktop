0c2f18db909883baa8f53e8a690c39b9760358aa
peter future <peter_future@outlook.com>
2014-05-14 20:22:30 +0800

    nw can play audio now

--- main.js
+++ main.js
@@ -1,10 +1,125 @@
+//===============================================
+// DTPLAYER Part
+//===============================================
+
+var ref = require('ref');
+var ffi = require('ffi');
+var Struct = require('ref-struct');
+var fs = require('fs');
+var dtplayer = require('dtplayer');
+
+// type def
+var int64_t = ref.types.int64;
+var dt_char = ref.types.char;
+var voidptr = ref.refType(ref.types.void);
+var uint8_ptr = ref.refType(ref.types.uint8);
+// structure def
+var dtp_state = Struct(
+{
+    cur_status:'int',
+    last_status:'int',
+    cur_time_ms:int64_t,
+    cur_time:int64_t,
+    full_time:int64_t,
+    start_time:int64_t
+}
+);
+var dtp_state_ptr = ref.refType(dtp_state);
+
+var dtp_para = Struct(
+{
+    file_name:'string',
+    video_index:'int',
+    audio_index:'int',
+    sub_index:'int',
+    loop_mode:'int',
+    no_audio:'int',
+    no_video:'int',
+    no_sub:'int',
+    sync_enable:'int',
+    width:'int',
+    height:'int',
+    update_cb:voidptr
+}
+);
+var dtp_para_ptr = ref.refType(dtp_para);
+
+var player_status = {
+    PLAYER_STATUS_INVALID:   -1,
+    PLAYER_STATUS_IDLE:       0,
+    
+    PLAYER_STATUS_INIT_ENTER: 1,
+    PLAYER_STATUS_INIT_EXIT:  2,
+
+    PLAYER_STATUS_START:      3,
+    PLAYER_STATUS_RUNNING:    4,
+
+    PLAYER_STATUS_PAUSED:     5,
+    PLAYER_STATUS_RESUME:     6,
+    PLAYER_STATUS_SEEK_ENTER: 7,
+    PLAYER_STATUS_SEEK_EXIT:  8,
+
+    PLAYER_STATUS_ERROR:      9,
+    PLAYER_STATUS_STOP:      10,
+    PLAYER_STATUS_PLAYEND:   11,
+    PLAYER_STATUS_EXIT:      12
+};
+
+//reg vo
+var canvas_vo = {
+    id:1000,
+    name:'canvas render',
+    vo_init:function(){
+        console.log('yeah, canvas init ok');
+    },
+    vo_stop:function(){
+        console.log('yeah, canvas stop ok');
+    },
+    vo_render:function(pic){
+        var picture = pic.deref();
+        console.log('canvas render one frame, pts'+ picture.pts);
+    }
+};
+
+var ply = new dtplayer();
+var dtp_cb = ffi.Callback('void',[dtp_state_ptr],function(state)
+{
+	var info = state.deref();
+    //ply.emit('update_info');
+
+    var sta;
+    switch(info.cur_status){
+        case player_status.PLAYER_STATUS_EXIT:
+             sta = '-exit-';
+             break;
+        case player_status.PLAYER_STATUS_RUNNING:
+             sta = '-playing-';
+             break;
+        case player_status.PLAYER_EVENT_PAUSE:
+             sta = '-pause-';
+             break;
+        case player_status.PLAYER_STATUS_SEEK_ENTER:
+             sta = '-seeking-';
+             break
+        default:
+             return '-unkown-';
+    };
+
+    console.log('cur time(s):' + info.cur_time + '  status:' + sta + '  full time:' + info.full_time);
+
+    if(info.cur_status == player_status.PLAYER_STATUS_EXIT)
+        ply.emit('play_end');
+});
+//===============================================
+// UI Part
+//===============================================
+
 var openButton, stopButton;
 var editor;
 var menu;
 var fileEntry;
 
 var gui = require("nw.gui");
-var fs = require("fs");
 var clipboard = gui.Clipboard.get();
 
 function playFile(theFileEntry) {
@@ -27,11 +142,34 @@ var onChosenFileToOpen = function() {
 
 
 function handleOpenButton() {
-  $("#openFile").trigger("click");
+  //$("#openFile").trigger("click");
+
+    console.log('here we want to play a video');
+    var url = '../1.aac';
+    var no_audio = -1;
+    var no_video = -1
+    var width = 720;
+    var height = 480;
+
+    var para = new dtp_para();
+    para.file_name = url;
+    para.no_audio = no_audio;
+    para.no_video = no_video;
+    para.width = width;
+    para.height = height;
+    para.update_cb = dtp_cb;
+
+    //reg vo
+    ply.reg_vo(canvas_vo);
+
+    ply.init(para);
+    ply.start();
+    
 }
 
 function handleStopButton() {
   $("#stopFile").trigger("click");
+  ply.stop();
 }
 
 function initContextMenu() {
