3946a2978d645a2b405c65c4d942c8eb116728c4
peter future <peter_future@outlook.com>
2014-05-19 09:46:56 +0800

    get canvas w-h dynamically

--- main.js
+++ main.js
@@ -67,6 +67,7 @@ var player_status = {
 
 var canvas = document.getElementById("video-render");
 var context = canvas.getContext("2d");
+
 var canvas_vo = {
     id:1000,
     name:'canvas render',
@@ -77,21 +78,23 @@ var canvas_vo = {
 		console.log('yeah, canvas stop ok');
     },
     vo_render:function(pic){
-
+        var x_start = 0;
+        var y_start = 0;
+        var width = canvas.width;
+        var height = canvas.height;
+        var rgb_lengh = width*height*3;
         var picture = pic.deref();
-        var imgdata = context.getImageData(0,0,720,480);
+        var imgdata = context.getImageData(x_start,y_start,width,height);
         var data = picture.data0;
-		var rgb_data = data.reinterpret(720*480*3);
-        for(i=0,j=0;i<720*480*3;i+=3,j+=4)
+		var rgb_data = data.reinterpret(rgb_lengh);
+        for(i=0,j=0;i<rgb_lengh;i+=3,j+=4)
         {
             imgdata.data[j] = rgb_data[i];
             imgdata.data[j+1] = rgb_data[i+1];
             imgdata.data[j+2] = rgb_data[i+2];
             imgdata.data[j+3] = 255;
         }
-        context.putImageData(imgdata,0,0);
-		//console.log('length:'+rgb_data.length +'data:'+rgb_data[0]+'-'+rgb_data[2]+'-'+rgb_data[1000000]);
-        //console.log('canvas render one frame, pts'+ picture.pts + "width *3 = "+picture.linesize0);
+        context.putImageData(imgdata,x_start,y_start);
         return 0;
     }
 };
